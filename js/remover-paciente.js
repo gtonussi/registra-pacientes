a22ff5fbbd842756ca31060f78dc77437721fe92
gtonussi <42251514+gtonussi@users.noreply.github.com>
2019-04-07 14:40:29 -0300

    Add files via upload

---
+++ js/remover-paciente.js
@@ -0,0 +1,12 @@
+console.log("O arquivo javascript remover-paciente foi carregado");
+
+var pacientes = document.querySelectorAll(".paciente");
+
+var tabela = document.querySelector("table");
+
+tabela.addEventListener("dblclick", function(event){
+  event.target.parentNode.classList.add("fadeOut");
+  setTimeout(function(){
+    event.target.parentNode.remove();
+  }, 500)
+});
